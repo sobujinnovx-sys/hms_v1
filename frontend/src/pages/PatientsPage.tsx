bc29c0f8b4414632a82fa8ac85c247de0e75f735
sobuj <mdsobujshu2001@gmail.com>
2026-01-09 01:37:10 +0600

    error fix1

--- frontend/src/pages/PatientsPage.tsx
+++ frontend/src/pages/PatientsPage.tsx
@@ -18,7 +18,7 @@ import {
   DialogActions,
 } from '@mui/material';
 import api from '@services/api';
-import { Patient } from '@types/index';
+import { Patient } from '@types';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
--- frontend/src/stores/authStore.ts
+++ frontend/src/stores/authStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { User } from '@types/index';
+import { User } from '@types';
 import api from '@services/api';
 
 interface AuthStore {
